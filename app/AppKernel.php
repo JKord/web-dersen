ff57b51a6b69f8ca9863fb50ff784e41b44c602a
Kordyuk Jura <jura@webkate.com>
2013-10-03 19:52:59 +0300

    Light and Shadow

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new GameOfThrones\MorsWestfordBundle\GameOfThronesMorsWestfordBundle(),
             new GameOfThrones\LegacyBundle\GameOfThronesLegacyBundle(),
+            new GameOfThrones\LightAndShadowBundle\GameOfThronesLightAndShadowBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'jura'))) {
---
+++ src/GameOfThrones/LightAndShadowBundle/GameOfThronesLightAndShadowBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GameOfThrones\LightAndShadowBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GameOfThronesLightAndShadowBundle extends Bundle
+{
+}
